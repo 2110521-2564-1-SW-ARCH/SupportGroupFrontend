0c81582fe5123f57d2490d3d69e541138492ed23
bosspichapong <42568992+bosspichapong@users.noreply.github.com>
2021-09-26 02:07:52 +0700

    ~change style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import {ToastContainer} from 'react-toastify';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import store from './redux';
 import PrivateRoute from './routes/private-route';
 import Signup from './pages/signup';
@@ -16,8 +16,8 @@ function App() {
       <Router>
         <ToastContainer position="top-left" />
         <Switch>
-          <Route exact path="/" component={Signup} />
-          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
           <PrivateRoute exact path="/chat" component={ChatRoom} />
           <PrivateRoute exact path="/home" component={Home} />
         </Switch>
